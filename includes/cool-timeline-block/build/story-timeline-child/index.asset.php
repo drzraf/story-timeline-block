425d69cfb763e638d0ebe3bcf1445f54070a8954
Raphaël Droz <raphael.droz@gmail.com>
2023-03-14 13:15:45 -0300

    build files

--- includes/cool-timeline-block/build/story-timeline-child/index.asset.php
+++ includes/cool-timeline-block/build/story-timeline-child/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => 'b81b2d32eafb758119c9');
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => '0890a1e1b4b1ae32ea61');
